ca13df052c19dca7a93ae2abc5e5d48d8c4d6dcb
oliver <icetzsr@gmail.com>
2024-04-15 19:47:47 +0800

    add more funs

--- DemoLibrary.cpp
+++ DemoLibrary.cpp
@@ -3,20 +3,53 @@
 #include "Arduino.h"
 #include "DemoLibrary.h"
 
-DemoLibrary::DemoLibrary(int ledPin, int baudrate) {
+DemoLibrary::DemoLibrary()
+{
+}
+// 初始化
+void DemoLibrary::init(int ledPin, int baudrate)
+{
   pinMode(ledPin, OUTPUT);
   _ledPin = ledPin;
   Serial.begin(baudrate); // 初始化串口
 }
 
-void DemoLibrary::ledblinky(int interval) {
+void DemoLibrary::ledblinky(int interval)
+{
   digitalWrite(_ledPin, HIGH);
   delay(interval);
   digitalWrite(_ledPin, LOW);
   delay(interval);
 }
 
-void DemoLibrary::helloworld() {
+void DemoLibrary::helloworld()
+{
   Serial.println("Hello World");
 }
 
+// add two int
+int DemoLibrary::add(int a, int b)
+{
+  return a + b;
+}
+// subtract two int
+int DemoLibrary::sub(int a, int b)
+{
+  if (a < b)
+  {
+    return b - a;
+  }
+  return a - b;
+}
+
+// find min from two int
+int DemoLibrary::min(int a, int b)
+{
+  return a < b ? a : b;
+}
+
+// find max from two int
+int DemoLibrary::max(int a, int b)
+{
+  return a > b ? a : b;
+}
--- DemoLibrary.h
+++ DemoLibrary.h
@@ -1,16 +1,23 @@
 
 #ifndef DemoLibrary_h
 #define DemoLibrary_h
-
+#if ARDUINO >= 100
 #include "Arduino.h"
+#endif
+class DemoLibrary
+{
+public:
+  DemoLibrary();
+  void init(int ledPin, int baudrate);
+  void ledblinky(int interval);
+  void helloworld();
+  int add(int a, int b);
+  int sub(int a, int b);
+  int max(int a, int b);
+  int min(int a, int b);
 
-class DemoLibrary {
-  public:
-    DemoLibrary(int ledPin, int baudrate);
-    void ledblinky(int interval);
-    void helloworld();
-  private:
-    int _ledPin;
+private:
+  int _ledPin;
 };
 
 #endif
